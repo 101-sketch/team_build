274ef4252b9c6b5e679371665d3eaf2ff1775d29
mise yuuki <yuuki.mise16@gmail.com>
2025-05-27 11:25:26 +0900

    画面あり

--- sample.java
+++ sample.java
@@ -1,15 +1,17 @@
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
 public class sample {
     public static void main(String[] args) {
-        String word_a = "朝から驚きの連続だ";
-        String word_i = "いつもの朝とは様子が違う";
-        String word_u = "嬉しい知らせが飛び込んできた";
-        String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "温泉旅行に出発だ！";
 
-        System.out.println("あ：" + word_a);
-        System.out.println("い：" + word_i);
-        System.out.println("う：" + word_u);
-        System.out.println("え：" + word_e);
-        System.out.println("お：" + word_o);
+        JFrame mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(800, 500);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setLayout(new GridLayout(2, 1));
+        mainFrame.setVisible(true);
+
+        
     }
+
 }
